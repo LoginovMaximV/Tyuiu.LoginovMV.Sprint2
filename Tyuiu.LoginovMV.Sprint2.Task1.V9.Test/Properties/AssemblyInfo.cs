ddfcde444cffa7b695613b5bbc0b8c75499a78a3
Максим Логинов <maxdompc@gmail.com>
2023-10-09 13:51:25 +0500

    Task1

---
+++ Tyuiu.LoginovMV.Sprint2.Task1.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LoginovMV.Sprint2.Task1.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LoginovMV.Sprint2.Task1.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("863dbaec-3e1a-4875-b0ca-fa24370c12e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.LoginovMV.Sprint2.Task1.V9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LoginovMV.Sprint2.Task1.V9.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint2.Task1.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2| Выполнил: Логинов М.В | ИИПб-23-2";
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                                  *");
+            Console.WriteLine("* Тема: Логические операции                                                                                  *");
+            Console.WriteLine("* Задание #1                                                                                                 *");
+            Console.WriteLine("* Вариант #9                                                                                                 *");
+            Console.WriteLine("* Выполнил: Логинов М.В   | ИИПб-23-2                                                                        *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                   *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,                                            *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз в выражении) и                               *");
+            Console.WriteLine("* логических операций (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться),                *");
+            Console.WriteLine("* а также арифметических выражений, которая вернет логическую последовательность(массив):                    *");
+            Console.WriteLine("* (False, False, True, False, True, False), при a = 15, b = 335, c = 174, d = 478                            *");
+            Console.WriteLine("**************************************************************************************************************");
+            int a = 15;
+            int b = 335;
+            int c = 174;
+            int d = 478;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                           *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("A=" + a);
+            Console.WriteLine("B=" + b);
+            Console.WriteLine("C=" + c);
+            Console.WriteLine("D=" + d);
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                 *");
+            Console.WriteLine("**************************************************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
