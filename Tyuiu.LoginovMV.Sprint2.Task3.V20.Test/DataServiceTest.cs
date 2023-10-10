f216976dcf2eb4b8c47b17b352e510f0058c94b3
Максим Логинов <maxdompc@gmail.com>
2023-10-10 14:05:36 +0500

    Task3

---
+++ Tyuiu.LoginovMV.Sprint2.Task3.V20.Test/DataServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LoginovMV.Sprint2.Task3.V20.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint2.Task3.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 18.000;
+            Assert.AreEqual(wait, res);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.750;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 2.000;
+            Assert.AreEqual(wait, res);
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            DataService ds = new DataService();
+            double x = -25;
+            double res = ds.Calculate(x);
+            double wait = -274.960;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.LoginovMV.Sprint2.Task3.V20/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LoginovMV.Sprint2.Task3.V20.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint2.Task3.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2| Выполнил: Логинов М.В | ИИПб-23-2";
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                                  *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                                                        *");
+            Console.WriteLine("* Задание #2                                                                                                 *");
+            Console.WriteLine("* Вариант #20                                                                                                *");
+            Console.WriteLine("* Выполнил: Логинов М.В   | ИИПб-23-2                                                                        *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                   *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с использованием вложенных              *");
+            Console.WriteLine("* оператор if-else, где пользователь вводит значение переменной X с клавиатуры.                              *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой;                                                *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                           *");
+            Console.WriteLine("**************************************************************************************************************");
+            int x;
+            Console.WriteLine("Введите значение Х:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                 *");
+            Console.WriteLine("**************************************************************************************************************");
+            double y;
+            y = ds.Calculate(x);
+            Console.WriteLine("Значение фунции="+ y);
+            Console.ReadKey();
+        }
+    }
+}
