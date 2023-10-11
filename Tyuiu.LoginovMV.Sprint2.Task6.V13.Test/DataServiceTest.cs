ebcd63b2aa77cd07421ba2a3578957ae488e1f92
Максим Логинов <maxdompc@gmail.com>
2023-10-11 13:19:44 +0500

    Task6.1

--- Tyuiu.LoginovMV.Sprint2.Task6.V13.Test/DataServiceTest.cs
+++ Tyuiu.LoginovMV.Sprint2.Task6.V13.Test/DataServiceTest.cs
@@ -11,11 +11,11 @@ namespace Tyuiu.LoginovMV.Sprint2.Task6.V13.Test
         public void TestMethod1()
         {
             DataService ds = new DataService();
-            int g = 2001;
+            int g = 2000;
             int m = 12;
             int n = 31;
             string res = ds.FindDateOfNextDay(g, m, n);
-            string wait = "2002.1.1";
+            string wait = "2001.1.1";
             Assert.AreEqual(wait, res);
         }
     }
